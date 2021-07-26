b6545e2985da3626cdc19fe843553b27740238f5
mlatus <wqseleven@gmail.com>
2021-07-26 10:36:58 +0800

    pci passthrough init

--- world-implementation/module/hardware/pci-passthrough.nix
+++ world-implementation/module/hardware/pci-passthrough.nix
@@ -4,9 +4,7 @@
       boot.loader.grub.configurationName = "PCI passthrough";
       powersave.enable = false;
       nvidia.asPrimaryGPU = true;
-      boot.kernelParams = [
-        "intel_iommu=on"
-        "iommu=pt"
-      ];
+      boot.kernelParams = [ "intel_iommu=on" "iommu=pt" ];
     };
+  };
 }
