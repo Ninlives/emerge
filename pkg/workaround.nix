f08a5deee19954867ddadefb08197ce31447ec21
mlatus <wqseleven@gmail.com>
2023-04-18 11:32:34 +0800

    fix pixel-saver

--- pkg/workaround.nix
+++ pkg/workaround.nix
@@ -2,9 +2,12 @@
 final: prev: {
   gnomeExtensions = prev.gnomeExtensions // {
     pixel-saver = prev.gnomeExtensions.pixel-saver.overrideAttrs (p: {
-      postUnpack = p.postUnpack or "" + ''
-        ${final.jq}/bin/jq -e '.["shell-version"] += ["44"]' < pixel-saver@deadalnix.me/metadata.json | \
-        ${final.moreutils}/bin/sponge pixel-saver@deadalnix.me/metadata.json
+      preInstall = p.preInstall or "" + ''
+        ${final.jq}/bin/jq -e '.["shell-version"] += ["44"]' < metadata.json | \
+        ${final.moreutils}/bin/sponge metadata.json
+
+        sed -e 's#/usr/bin/xprop#${final.xorg.xprop}/bin/xprop#' \
+            -e "s#'xprop#'${final.xorg.xprop}/bin/xprop#" -i decoration.js
       '';
     });
   };
