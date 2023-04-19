087c99f72fd83e3a806804957eaa6696d2f75fdf
mlatus <wqseleven@gmail.com>
2023-04-19 09:17:46 +0800

    revert changes

--- impl/lego/hardware/firmware-driver.nix
+++ impl/lego/hardware/firmware-driver.nix
@@ -1,40 +1,40 @@
-{ pkgs, inputs, lib, ... }: {
+{ pkgs, inputs, ... }: {
 
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
-  # Temporary
-  imports = [
-    "${
-      inputs.nixpkgs.legacyPackages.x86_64-linux.applyPatches {
-        name = "jovian";
-        src = inputs.jovian;
-        patches = builtins.toFile "fhs.patch" ''
-          diff --git a/modules/steam.nix b/modules/steam.nix
-          index f726a8b..d1ddb11 100644
-          --- a/modules/steam.nix
-          +++ b/modules/steam.nix
-          @@ -31,7 +31,7 @@ let
-             # can't run a binary with such a capability without being Setuid
-             # itself.
-             steam = pkgs.steam.override {
-          -    buildFHSUserEnv = pkgs.buildFHSUserEnvBubblewrap.override {
-          +    buildFHSEnv = pkgs.buildFHSEnv.override {
-                 bubblewrap = "''${config.security.wrapperDir}/..";
-               };
-             };
-        '';
-      }
-    }/modules"
-  ];
-  nixpkgs.overlays = [
-    (final: prev: {
-      mangohud = prev.mangohud.overrideAttrs (p: {
-        patches = builtins.filter
-          (pa: !(lib.hasSuffix "preload-nix-workaround.patch" pa))
-          (p.patches or [ ]);
-      });
-    })
-  ];
+  imports = [ "${inputs.jovian}/modules" ];
+  # imports = [
+  #   "${
+  #     inputs.nixpkgs.legacyPackages.x86_64-linux.applyPatches {
+  #       name = "jovian";
+  #       src = inputs.jovian;
+  #       patches = builtins.toFile "fhs.patch" ''
+  #         diff --git a/modules/steam.nix b/modules/steam.nix
+  #         index f726a8b..d1ddb11 100644
+  #         --- a/modules/steam.nix
+  #         +++ b/modules/steam.nix
+  #         @@ -31,7 +31,7 @@ let
+  #            # can't run a binary with such a capability without being Setuid
+  #            # itself.
+  #            steam = pkgs.steam.override {
+  #         -    buildFHSUserEnv = pkgs.buildFHSUserEnvBubblewrap.override {
+  #         +    buildFHSEnv = pkgs.buildFHSEnv.override {
+  #                bubblewrap = "''${config.security.wrapperDir}/..";
+  #              };
+  #            };
+  #       '';
+  #     }
+  #   }/modules"
+  # ];
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     mangohud = prev.mangohud.overrideAttrs (p: {
+  #       patches = builtins.filter
+  #         (pa: !(lib.hasSuffix "preload-nix-workaround.patch" pa))
+  #         (p.patches or [ ]);
+  #     });
+  #   })
+  # ];
 
   jovian.devices.steamdeck.enable = true;
 
