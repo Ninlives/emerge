b61d294fd9b2af7e1cd2b06b6a691776ad3f73e9
mlatus <wqseleven@gmail.com>
2023-04-26 22:22:45 +0800

    enable ungoogled-chromium, WeTab extension, ltex-ls

--- impl/lego/network/browser.nix
+++ impl/lego/network/browser.nix
@@ -1,12 +1,19 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   programs.chromium = {
     enable = true;
     extensions = [
       "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
       "padekgcemlokbadohgkifijomclgjgif" # Proxy SwitchyOmega
+      "aikflfpejipbpjdlfabpgclhblkpaafo" # WeTab
     ];
   };
-  environment.systemPackages = [ pkgs.chromium ];
+  environment.systemPackages = [ (pkgs.ungoogled-chromium.override { commandLineArgs = lib.concatStringsSep " " [
+    "--disable-search-engine-collection"
+    "--fingerprinting-canvas-image-data-noise"
+    "--fingerprinting-canvas-measuretext-noise"
+    "--fingerprinting-client-rects-noise"
+    "--enable-features=DisableLinkDrag"
+  ]; }) ];
 
   revive.specifications.user.boxes = [{
     src = /Programs/chromium;
--- impl/neko/program/neovim/settings/lsp.nix
+++ impl/neko/program/neovim/settings/lsp.nix
@@ -1,15 +1,15 @@
-{ pkgs, lib, config, fn, nixosConfig, ... }:
+{ pkgs, lib, ... }:
 let
-  inherit (pkgs) nil texlab pyright;
+  inherit (pkgs) nil texlab pyright ltex-ls;
   inherit (pkgs.nodePackages) bash-language-server;
   inherit (lib.hm.dag) entryAfter;
 in {
   programs.neovim.settings.lsp = entryAfter [ "cmp" ] {
     plugins = p: with p; [ nvim-lspconfig ];
-    extraPackages = [ nil bash-language-server texlab pyright ];
+    extraPackages = [ nil bash-language-server texlab pyright ltex-ls ];
 
     lua = /* lua */ ''
-      local lsps = { 'nil_ls', 'bashls', 'pyright', 'texlab' }
+      local lsps = { 'nil_ls', 'bashls', 'pyright', 'texlab', 'ltex'}
       local lspconfig = require('lspconfig')
       for _, lsp in pairs(lsps) do
         lspconfig[lsp].setup {
