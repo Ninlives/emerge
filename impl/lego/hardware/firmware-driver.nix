0b1e4a761f3c7a2b399a4e9209c28c12393867e9
mlatus <wqseleven@gmail.com>
2023-04-30 15:27:47 +0800

    remove patches for jovian

--- impl/lego/hardware/firmware-driver.nix
+++ impl/lego/hardware/firmware-driver.nix
@@ -2,37 +2,7 @@
 
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
-  # imports = [ "${inputs.jovian}/modules" ];
-  imports = let nixpkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-  in [
-    "${
-      nixpkgs.applyPatches {
-        name = "jovian";
-        src = inputs.jovian;
-        patches = [
-          (nixpkgs.fetchpatch {
-            url =
-              "https://github.com/Jovian-Experiments/Jovian-NixOS/commit/dc365da27a3119a2ba1e858c46d7a67edae1822d.patch";
-            sha256 = "sha256-1167i4aYe3MvpRlDXuigGn0nvbubz9HJXCboOEv4A+M=";
-          })
-          (nixpkgs.fetchpatch {
-            url =
-              "https://github.com/Jovian-Experiments/Jovian-NixOS/commit/7b754a9c444085a78386823d8ff23097cf9b7ae3.patch";
-            sha256 = "sha256-+QkuDE1OEH3AyKxXxCRVRDtEmXep4Ub0rW/gtFSuL5U=";
-          })
-        ];
-      }
-    }/modules"
-  ];
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     mangohud = prev.mangohud.overrideAttrs (p: {
-  #       patches = builtins.filter
-  #         (pa: !(lib.hasSuffix "preload-nix-workaround.patch" pa))
-  #         (p.patches or [ ]);
-  #     });
-  #   })
-  # ];
+  imports = [ "${inputs.jovian}/modules" ];
 
   jovian.devices.steamdeck.enable = true;
 
