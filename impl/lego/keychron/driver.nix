f022a0df0a1b0cf0e467970ef6060097885cf53d
mlatus <wqseleven@gmail.com>
2023-10-26 15:25:02 +0800

    keychron udev rules

--- impl/lego/keychron/driver.nix
+++ impl/lego/keychron/driver.nix
@@ -3,4 +3,8 @@
      unusable in Steam */
   # services.udev.packages = [ pkgs.via ];
   # hardware.keyboard.qmk.enable = true;
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="02c0", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  '';
 }
