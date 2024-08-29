5a694a264dd25c53fc135b31e21334215daabdba
mlatus <wqseleven@gmail.com>
2024-08-29 15:50:44 +0800

    update vps

--- impl/echo/misskey/default.nix
+++ impl/echo/misskey/default.nix
@@ -25,9 +25,6 @@ in {
       webserver.nginx = {
         enableACME = true;
         forceSSL = true;
-        locations."/".extraConfig = ''
-          proxy_redirect off;
-        '';
       };
     };
   };
