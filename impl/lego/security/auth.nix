70383a09e239ebdf68b1cece07b17d846b65e6b2
mlatus <wqseleven@gmail.com>
2025-01-13 17:32:53 +0800

    only downgrade pcsclite in lego

--- impl/lego/security/auth.nix
+++ impl/lego/security/auth.nix
@@ -1,5 +1,20 @@
 {pkgs, ...}: {
   services.pcscd.enable = true;
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        inherit
+          (import (final.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "2d2a9ddbe3f2c00747398f3dc9b05f7f2ebb0f53";
+            hash = "sha256-B5WRZYsRlJgwVHIV6DvidFN7VX7Fg9uuwkRW9Ha8z+w=";
+          }) {inherit (final) system;})
+          pcsclite
+          ;
+      }
+    )
+  ];
 
   services.udev.packages = [pkgs.yubikey-personalization];
   systemd.packages = [pkgs.yubikey-touch-detector];
