efaa5ee4748648f9b13d4c07ae76c122deb0ba56
mlatus <wqseleven@gmail.com>
2025-08-26 22:56:15 +0800

    enable ratbagd

--- impl/lego/d-desktop-hardware/driver.nix
+++ impl/lego/d-desktop-hardware/driver.nix
@@ -2,4 +2,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
   boot.kernelModules = ["kvm-amd" "btintel"];
+
+  services.ratbagd.enable = true;
 }
