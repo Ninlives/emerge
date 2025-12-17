c93c823fceb263173a4461bd71712128680caea2
mlatus <wqseleven@gmail.com>
2025-12-17 21:08:08 +0800

    adaption

--- impl/lego/d-desktop-hardware/driver.nix
+++ impl/lego/d-desktop-hardware/driver.nix
@@ -2,7 +2,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
   boot.kernelModules = ["kvm-amd" "btintel"];
-  hardware.graphics.extraPackages = [pkgs.libvdpau-va-gl pkgs.vaapiVdpau];
+  hardware.graphics.extraPackages = [pkgs.libvdpau-va-gl pkgs.libva-vdpau-driver];
 
   services.ratbagd.enable = true;
 }
