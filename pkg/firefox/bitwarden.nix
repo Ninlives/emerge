6cc5d1c610bb3842af5b793cdf8bd406d5cb83d0
mlatus <wqseleven@gmail.com>
2026-01-06 01:02:57 +0800

    fix hash

--- pkg/firefox/bitwarden.nix
+++ pkg/firefox/bitwarden.nix
@@ -12,7 +12,7 @@ buildNpmPackage rec {
     rev = "d9d8b4ba55809858dc3ce2d8b1ea512253abe678";
     sha256 = "sha256-i+hLslZ2i94r04vaOzx9e55AR8aXa9sSK8el+Dcp05A=";
   };
-  npmDepsHash = "sha256-44444444444444444444444444aXa9sSK8el+Dcp05A=";
+  npmDepsHash = "sha256-OT9Ll+F4e/yOJVpay/zwfEHcBqRvSFOM2mtlrJ8E6fs=";
   npmWorkspace = "apps/browser";
   makeCacheWritable = true;
   npmFlags = ["--legacy-peer-deps"];
