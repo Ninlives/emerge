d3c50a5479675753402f8099596ae308860137e1
mlatus <wqseleven@gmail.com>
2026-02-26 13:45:42 +0800

    disable assertion

--- impl/lego/m-security/auth.nix
+++ impl/lego/m-security/auth.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  assertions = [{ assertion = pkgs.pcsclite.version == "2.3.0"; }];
+  # assertions = [{ assertion = pkgs.pcsclite.version == "2.3.0"; message = "pcsclite updated: ${pkgs.pcsclite.version}"; }];
   services.pcscd.enable = true;
 
   services.udev.packages = [pkgs.yubikey-personalization];
